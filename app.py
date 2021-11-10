517bd8ce28cf8a1b4611240517a9b189d2f6c20d
sushantsur23 <sushant.sur@gmail.com>
2021-11-10 23:43:49 +0530

    first commit

--- app.py
+++ app.py
@@ -62,6 +62,6 @@ def processRequest(req):
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    print("Starting app on port %d" % port)
-    #app.run(debug=True)
+    #port = int(os.getenv('PORT', 5000))
+    #print("Starting app on port %d" % port)
+    app.run(debug=True)
